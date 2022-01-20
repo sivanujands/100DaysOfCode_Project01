1cc8eb2fd00a9f3dc35b093a458c89e75c54c318
sivanujands <87688172+sivanujands@users.noreply.github.com>
2022-01-20 10:24:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Student ID Generator")
+
+let_1st=input("what's your initial charecter of  your name?\n")
+
+reg_no=input("what's your registration number?\n")
+
+print("Your Student ID could be " + let_1st+ reg_no)
